b1ab03b62331078b529a319a9a50c1e4e8621991
Anna Podchasova <pod9696@gmail.com>
2022-10-20 22:57:44 +0300

    first comment

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа по определению четного или нечетного числа!");
+            Console.WriteLine("Введите число:");
+
+            int N = int.Parse(Console.ReadLine());
+
+            int remainder = N % 2;
+            if (remainder == 0)
+                Console.WriteLine("Вы ввели четное число");
+            else
+                Console.WriteLine("Вы ввели нечетное число");
+
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
